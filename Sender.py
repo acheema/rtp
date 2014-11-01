0decb4a70f514d835a1c3946f74f457bc1ddf64d
akaljot <akaljot.cheema@gmail.com>
2014-11-01 15:56:59 -0700

    Got sliding window, passes basic test + fast retransmit, no SACK

--- Sender.py
+++ Sender.py
@@ -1,82 +1,88 @@
 import sys
 import getopt
 
+
 import Checksum
 import BasicSender
 
 '''
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
+
 class Sender(BasicSender.BasicSender):
+
     def __init__(self, dest, port, filename, debug=False, sackMode=False):
         super(Sender, self).__init__(dest, port, filename, debug)
-        if sackMode:
-            raise NotImplementedError #remove this line when you implement SACK
-    
+        self.sentPkts = {}
+        self.ackPkts = {}
+        self.globalseq = 0
+        self.done = False
+        #if sackMode:
+            #raise NotImplementedError
+
     def start(self):
-        seqnum = 0
-        sentPkts = []
-        ackPkts = []
-        msgIter = self.infile.read(1400)
-        msg_type = 'start'
-        next_msg = None
-        while not msg_type == 'end':
-            if next_msg != "":
-                packet = self.make_packet(msg_type,seqnum,msgIter)
-                sentPkts.append(seqnum)
-                self.send(packet)
-                #print "--------------------------------------------------------"
-                print "sent: %s|%d|%s|" % (msg_type, seqnum, msgIter[:5])
-                
-                response = self.receive(.5)
-                #ackPkts.append(int(float(self.split_packet(response)[1])))
-                if self.handle_response(response):
-                    next_msg = self.infile.read(1400)
-                    if next_msg == "":
-                        continue
-                    msg_type = 'data'
-                    msgIter = next_msg
-                    seqnum += 1
+        msg_type = None
+        sent_type = None
+        while not self.done:
+            while not len(self.sentPkts) >= 5 and sent_type != 'end':
+                sent_type = self.send_data()
+            response = self.receive(0.5)		
+            print "recv: %s" % response
+            #Using the handle_response from examples
+            if Checksum.validate_checksum(response):
+                ackSeqNum = int(float(self.split_packet(response)[1]))
+                if ackSeqNum not in self.ackPkts:
+                    self.ackPkts[ackSeqNum] = 0
+                    self.handle_new_ack(ackSeqNum)
                 else:
-                    continue
-            else:
-                msg_type = 'end'
-                packetLast = self.make_packet(msg_type, seqnum, msgIter)
-                self.send(packetLast)
-                print "sent: %s|%d|%s|" % (msg_type, seqnum, msgIter[:5])
-                responseLast= self.receive(.5)
-                if self.handle_response(responseLast):
-                    return
-                else:
-                    continue
-        #print sentPkts
-        #print ackPkts
+                    self.ackPkts[ackSeqNum] += 1
+                    #fast retransmit
+                    if self.ackPkts[ackSeqNum] == 3:
+                        self.handle_dup_ack(ackSeqNum)
+            if response == None:
+                    self.handle_timeout()
+        if len(self.sentPkts) == 0:
+            self.done = True
         self.infile.close()
-        
-    #This kind of takes care of corruption
-    def handle_response(self,response_packet):
-        if Checksum.validate_checksum(response_packet):
-            print "recv: %s" % response_packet
-            #print "--------------------------------------------------------"
-            return True
-        else:
-            print "recv: %s <--- CHECKSUM FAILED" % response_packet
-            #print "--------------------------------------------------------"
-            return False
 
     def handle_timeout(self):
-        pass
+        for s in self.sentPkts:
+            self.send(self.sentPkts[s])
 
     def handle_new_ack(self, ack):
-        pass
+        for s in self.sentPkts.keys():
+            if s < ack:
+                del self.sentPkts[s]
+		del self.ackPkts[s]
+        if not len(self.sentPkts) >= 5:
+            msg_type = self.send_data()
+            if msg_type == 'end':
+                self.done = True
 
     def handle_dup_ack(self, ack):
-        pass
+	self.send(self.sentPkts[ack])
 
     def log(self, msg):
         if self.debug:
             print msg
 
+    def send_data(self):
+        msg = self.infile.read(1372)
+        if self.globalseq == 0:
+            msg_type = 'start'
+        elif len(msg) == 0:
+            msg_type = 'end'
+            msg = ""
+        else:
+            msg_type = 'data'
+        currseq = self.globalseq
+        self.globalseq += 1
+        packet = self.make_packet(msg_type, currseq, msg)
+        self.ackPkts[currseq] = 0
+        self.sentPkts[currseq] = packet
+        self.send(packet)
+        print "sent: %s|%d|%s|" % (msg_type, currseq, msg[:5])
+        return msg_type
 
 '''
 This will be run if you run this script from the command line. You should not
